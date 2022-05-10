1f451b8bb946fbef2ec20da101c4d66c8ed69f9e
Federico Heredia <federicomartinheredia2006@gmail.com>
2022-05-10 12:51:22 -0300

    first commit

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+   int a = atoi(argv[1]);
+   int b = atoi(argv[2]);
+   int c = atoi(argv[3]);
+  if (a == b, b == c) {
+    printf("Iguales");
+  }
+  else
+  printf("No iguales");
+  return 0;
+}
